f7a0caf738f2b27546297d023810e118663593c5
elop123 <142408289+elop123@users.noreply.github.com>
2024-09-04 10:50:56 +0200

    validation_input

---
+++ src/components/Form.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react"
+
+export const Form=()=>{
+
+    const[name, setName] = useState("");
+    const[error, setError] = useState(" ");
+    const[email, setEmail] =useState("");
+   
+    
+    console.log(name);
+    console.log(email);
+
+useEffect(()=>{
+const specialSigns = /[!@#$%^&*(),.?":{}|<>]/;   
+
+if(name.length === 0){
+    setError('')
+}
+else if (name.length < 6 && !specialSigns.test(name)) {
+    setError('Username should contain at least 6 characters and one special sign');
+} else {
+    setError('');
+        }
+},[name])
+
+useEffect(()=>{
+    const validEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (email.length === 0){
+        setError('')
+    }
+    else if(validEmail.test(email)){
+        setError('');
+    }
+    else{
+        setError('Your email is not valid');
+    }
+},[email])
+
+    return(
+    <section>
+    <form>
+        <label htmlFor="Username">Username</label>
+        <input onChange={(e)=>setName(e.target.value)} 
+               type="text" 
+               value={name} />
+        <label htmlFor="Email">Email</label>
+        <input onChange={(e)=>setEmail(e.target.value)} 
+               type="email" 
+               value={email} />
+      </form>
+      <b style={{color:'red'}}>{error} </b>
+    </section>)
+}
